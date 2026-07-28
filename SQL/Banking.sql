75230b09d7803031544d727468b9ce6a551ff688
omsupare <omsupare123@gmail.com>
2026-07-28 08:47:33 +0530

    Learn't about Data Types and created a table

--- SQL/Banking.sql
+++ SQL/Banking.sql
@@ -6,3 +6,25 @@ USE BankingDB;
 
 -- DOUBLE CLICK KARKE BHI USE KAR SKTE HAI
 
+-- DataTypes :
+-- cloud or aws , mysql official documentation
+-- 1. Tiny int
+-- 2. Small Int
+-- 3. Big Int
+
+CREATE TABLE Customers (
+	CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccountCreationDate DATE
+);
+
+DESCRIBE Customers;
+
+SELECT * FROM Customers;
+
+-- INSERT (CustomerID) VALUES INTO Customers ; 
+
+
